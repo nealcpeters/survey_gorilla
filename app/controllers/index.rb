eb7f3c90b81a45c739dff3f2ab873740d192f24d
thejessleigh <j.l.unrein@gmail.com>
2014-02-28 10:55:26 -0600

    Created basic controller structure for the first few pages
    Have login, index, create survey, create question
    
    View for create survey.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,39 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  if session[:user_id] == nil
+    erb :login
+  else
+    erb :index
+  end
+end
+
+
+
+get '/survey/create' do
+  if session[:user_id] == nil
+    redirect '/'
+  end
+
+  erb :create_survey
+end
+
+#################################################POST
+
+
+post '/login' do
+  @user = User.find_by(params[:email])
+  if @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = true
+    erb :login
+  end
+end
+
+post '/survey/create' do
+  @survey = Survey.create(title: params[:title], user: session[:user_id], description: params[:description], public?: [:public])
+end
+
+post '/survey/question/create' do
+  @survey.questions << Question.new(params)
 end
