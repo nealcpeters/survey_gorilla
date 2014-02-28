b71ac638251da68bbecb0f413e44a30a18c9e5cf
nealcpeters <nealcpeters@gmail.com>
2014-02-28 13:26:16 -0600

    added validations

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,4 @@
 class Option < ActiveRecord::Base
+  validates :text, presence: :true
   has_one :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  validates :text, presence: :true
+
   has_one :survey
   has_many :options
   has_many :answers, through: :options
